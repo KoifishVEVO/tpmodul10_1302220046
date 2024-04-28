3955407e726489eadcb6ec755316a7b8093935a0
KoifishVEVO <farhan210604@gmail.com>
2024-04-28 16:20:28 +0700

    menambahkan API mahasiswa

---
+++ tpmodul10_1302220046/Controllers/Mahasiswa.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace tpmodul10_1302220046.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        // Static list to store Mahasiswa data
+        private static List<Mahasiswa> mahasiswaList = new List<Mahasiswa>
+        {
+            new Mahasiswa ("Muhammad Farhan Ismali Fentarto", "1302220046" ),
+            new Mahasiswa ( "Kevin Albany Junaidi", "1302223027" ),
+            new Mahasiswa ( "Adrian Fahren Setiawan", "1302220018" ),
+            new Mahasiswa ( "Rindang Bani Isyan", "1302223023" ),
+            new Mahasiswa ( "Adib Faizulhaq Armadhani", "1302223110" ),
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mahasiswaList;
+        }
+
+        [HttpGet("{index}")]
+        public IActionResult Get(int index)
+        {
+            if (index >= 0 && index < mahasiswaList.Count)
+            {
+                return Ok(mahasiswaList[index]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Mahasiswa mahasiswa)
+        {
+            mahasiswaList.Add(mahasiswa);
+            return CreatedAtAction(nameof(Get), new { index = mahasiswaList.Count - 1 }, mahasiswa);
+        }
+
+        [HttpDelete("{index}")]
+        public IActionResult Delete(int index)
+        {
+            if (index >= 0 && index < mahasiswaList.Count)
+            {
+                mahasiswaList.RemoveAt(index);
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ tpmodul10_1302220046/Mahasiswa.cs
@@ -0,0 +1,12 @@
+﻿public class Mahasiswa
+{
+    public string Name { get; set; }
+    public string Nim { get; set; }
+
+    public Mahasiswa(string Name, string Nim)
+    {
+        this.Name = Name;
+        this.Nim = Nim;
+    }
+
+}
